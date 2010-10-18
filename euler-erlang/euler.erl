48b6b0eeda0c5b2ee05b7687f5bb0d2a28fc0f9e
Norbert Wójtowicz <wojtowicz.norbert@gmail.com>
2010-10-18 18:22:11 +0200

    euler.erl: fixed group/1, group_by/2; added for_each_line_in_file/4

--- euler-erlang/euler.erl
+++ euler-erlang/euler.erl
@@ -1,18 +1,24 @@
 -module(euler).
 
--export([group/1, pow/2]).
+-export([group/1, group_by/2, pow/2, for_each_line_in_file/4]).
 
-group([]) ->
+group(XS) ->
+    group_by(fun(A,B) -> A =:= B end, XS).
+
+group_by(_, []) ->
     [];
-group([X|XS]) ->
-    group(XS, [{X, 1}]).
+group_by(F, [X|XS]) ->
+    group_by(F, XS, [[X]]).
 
-group([], Acc) ->
+group_by(_, [], Acc) ->
     lists:reverse(Acc);
-group([X|YS], [{X, N}|ZS]) ->
-    group(YS, [{X, N+1}|ZS]);
-group([Y|YS], [{X, N}|ZS]) ->
-    group(YS, [{Y, 1}, {X, N} | ZS]).
+group_by(F, [X|XS], [[Y|YS]|ZS]) ->
+    case F(X, Y) of
+        true  -> 
+            group_by(F, XS, [[X,Y|YS]|ZS]);
+        false ->
+            group_by(F, XS, [[X], lists:reverse([Y|YS]) | ZS])
+    end.
 
 pow(X, N) when is_integer(N), N >= 0 -> pow(X, N, 1);
 pow(X, N) when is_integer(N) -> 1 / pow(X, -N, 1);
@@ -22,3 +28,15 @@ pow(_, 0, P) -> P;
 pow(X, N, A) when N rem 2 =:= 0 ->
     pow(X * X, N div 2, A);
 pow(X, N, A) -> pow(X, N - 1, A * X).
+
+
+for_each_line_in_file(Name, Proc, Mode, Accum0) ->
+    {ok, Device} = file:open(Name, Mode),
+    for_each_line(Device, Proc, Accum0).
+
+for_each_line(Device, Proc, Accum) ->
+    case io:get_line(Device, "") of
+        eof  -> file:close(Device), Accum;
+        Line -> NewAccum = Proc(Line, Accum),
+                    for_each_line(Device, Proc, NewAccum)
+    end.
